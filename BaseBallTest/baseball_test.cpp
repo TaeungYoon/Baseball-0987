9c5b6e066f87dd0280192c9bc106deb5b083d434
taeung.yoon <ytw0201@gmail.com>
2024-07-03 14:55:37 +0900

    [refactoring] page 32

--- BaseBallTest/baseball_test.cpp
+++ BaseBallTest/baseball_test.cpp
@@ -1,6 +1,14 @@
 #include "pch.h"
 #include "../BaseBallKata/baseball.cpp"
 
+class BaseballFixture : public testing::Test {
+public:
+	Baseball game;
+	void assertIllegalArgument(string guessNumber) {
+		//game.guess() 수행 후, exception이 발생해야 PASS 이다
+	}
+};
+
 TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
 	Baseball game;
 	EXPECT_THROW(game.guess(string("12")), length_error);
